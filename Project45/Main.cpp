6b4be550042774afb541baf5ee2f4e4fed8f36aa
ITSTEP\st <st>
2024-02-29 17:40:17 +0300

    Add project files.

---
+++ Project45/Main.cpp
@@ -0,0 +1,37 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	//const int n = 10;
+//	int size;
+//
+//	do {
+//		cout << "Input size of array: ";
+//		cin >> size;
+//	} while (size <= 0);
+//
+//	int* array = new int[size];
+//	int* first = array;
+//
+//	for (int i = 0; i < size; i++)
+//	{
+//		*(array++) = rand() % 100;
+//	}
+//
+//	for (int i = 0; i < size; i++)
+//	{
+//		cout << *(--array) << " ";
+//	}
+//
+//	cout << endl;
+//
+//	for (int i = 0; i < size; i++)
+//	{
+//		cout << *(array + i) << " ";
+//	}
+//
+//	delete[] first;
+//
+//	return 0;
+//}
---
+++ Project45/test03.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	const int size = 10;
+
+	int array[size];
+
+	for (int i = 0; i < size; i++)
+	{
+		*(array+i) = rand() % 100;
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << *(array+i) << " ";
+	}
+
+	cout << endl;
+
+	for (int i = size-1; i >= 0; i--)
+	{
+		cout << *(array + i) << " ";
+	}
+
+	return 0;
+}
